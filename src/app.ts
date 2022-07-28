c710b035e99e3fd1f4bc7140bf5be10be67056e2
mikiyaaa <katocha.m0220thankyou@gmail.com>
2022-07-28 18:48:56 +0900

    タスクボードに新規タスクを追加

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,58 @@
+/**************************** Taskのタイプを定義するクラス ****************************/
+enum TaskStatus {
+  Active, Finished
+}
+
+class Task {
+  constructor(public id: string, public title: string, public description: string, public manDay: number, public status: TaskStatus) {
+  }
+}
+
+/**************************** 状態管理用のクラス ****************************/
+type Listener = (items: Task[]) => void;
+
+class TaskState {
+  private _projects: Task[];
+  private _listeners: Listener[];
+  private static _instance: TaskState;
+  
+  // privateコンストラクタ => シングルトンなクラス
+  private constructor() {
+    this._projects = [];
+    this._listeners = [];
+  }
+
+  static getInstance() {
+    if (this._instance) {
+      return this._instance;
+    }
+    this._instance = new TaskState();
+    return this._instance;
+  }
+
+  addListener(listenerFunc: Listener) {
+    this._listeners.push(listenerFunc);
+  }
+
+  addTask(title: string, description: string, manDay: number) {
+    const newTask = new Task(
+      Math.random().toString(),
+      title,
+      description,
+      manDay,
+      TaskStatus.Active  // 新規追加はActive
+    );
+    this._projects.push(newTask);
+
+    for (const lisFnc of this._listeners) {
+      lisFnc(this._projects.slice()); // slice()メソッドで配列のコピーを渡す
+    }
+  }
+}
+// ProjectStateはシングルトンのクラス
+const projectState = TaskState.getInstance();  // staticメソッドを呼び出す
+
+
 /** バリデーションされるオブジェクトの型 */
 interface ValidationObj {
   value: string | number;
@@ -29,9 +84,63 @@ function validate(target: ValidationObj): boolean {
   return isValid;
 }
 
+/**************************** リストの中のProjectを表示するクラス ***************************FF*/
+
+
+/**************************** Projectのリスト（タスクボード）を表示するクラス ****************************/
+class TaskBoard {
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  sectionEl: HTMLElement;
+  assignedProjects: Task[]; // プロジェクトの配列を保存するためのプロパティ
+
+  constructor(private _type: 'active' | 'finished') {
+    this.templateEl = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostEl = document.getElementById('app')! as HTMLDivElement;
+    this.assignedProjects = [];
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.sectionEl = importedNode.firstElementChild as HTMLElement;
+    this.sectionEl.id = `${this._type}-projects`;
+
+    // 新規タスク追加時に、呼び出される
+    projectState.addListener((projects: Task[]) => {
+      this.assignedProjects = projects;
+      this.renderProjects();
+    })
+
+    this.attach();  // sectionブロックを描画
+    this.renderContent(); // sectionブロック内要素（h2・ul）への描画
+  }
+
+  private renderProjects() {
+    const ulEl = document.getElementById(`${this._type}-projects-list`)! as HTMLUListElement;
+    for (const prjItem of this.assignedProjects) {
+      const liEl = document.createElement('li');
+      liEl.textContent = prjItem.title;
+      ulEl.appendChild(liEl);
+    }
+  }
+
+  private renderContent() {
+    // section要素の中のh2要素にテキストを追加
+    const h2El = this.sectionEl.querySelector('h2')!;
+    h2El.textContent = this._type === 'active' ? '実行中プロジェクト' : '完了プロジェクト'; 
+
+    // section要素の中のul要素にid属性を追加
+    const ulEl = this.sectionEl.querySelector('ul')!;
+    ulEl.id = `${this._type}-projects-list`;
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement('beforeend', this.sectionEl); // 終了タグの前
+  }
+
+}
+/**************************** Formの表示・入力値の取得を行うクラス ****************************/
 class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
   element: HTMLFormElement;
   // 入力フォームへの参照
   titleInputElement: HTMLInputElement;
@@ -40,11 +149,11 @@ class ProjectInput {
 
 
   constructor() {
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.templateEl = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostEl = document.getElementById('app')! as HTMLDivElement;
 
     // templateタグ（子孫要素も含む）をインポート・複製
-    const importedNode = document.importNode(this.templateElement.content, true);
+    const importedNode = document.importNode(this.templateEl.content, true);
     // 最初の子要素をHTMLFormElement型で代入
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
@@ -53,7 +162,6 @@ class ProjectInput {
     this.titleInputElement =  this.element.querySelector('#title') as HTMLInputElement;
     this.descriptionInputElement =  this.element.querySelector('#description') as HTMLInputElement;
     this.manDayInputElement =  this.element.querySelector('#manday') as HTMLInputElement;
-
 
     this.configure();
     this.attach();
@@ -104,7 +212,8 @@ class ProjectInput {
     const inputValues = this.gatherUserInput();
     if (Array.isArray(inputValues)) {
       const [title, description, manDay] = inputValues;
-      console.log(title, description, manDay);
+
+      projectState.addTask(title, description, manDay);
       this.clearInput();
     }
   } 
@@ -116,8 +225,10 @@ class ProjectInput {
 
   /* 要素を追加 */
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    this.hostEl.insertAdjacentElement('afterbegin', this.element);
   }
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new TaskBoard('active');
+const finishedPrjList = new TaskBoard('finished');
